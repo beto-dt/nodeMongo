4b1c5a704dd61f97d4b0815ed226a52f93b9dec2
beto-dt <lucho.mix34@gmail.com>
2020-07-15 01:46:00 -0500

    adding data customer

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -2,9 +2,29 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const router = express.Router();
+const CustomerModel = mongoose.model('Customer');
 
-router.get('/',(req, res)=>{
-     res.send('Customer Controller');
+router.get('/list',(req, res)=>{
+    //Setting
+    var customer = new CustomerModel();
+    customer.CustomerName ='Tito';
+    customer.CustomerId ='2',
+    customer.save();
+
+    //Getting
+    CustomerModel.find((err,docs)=>{
+        if(!err)
+        {  
+            console.log(docs);
+            res.send('Customer Controller');
+        }
+        else
+        {
+            res.send("Error");
+        }
+    });
+
+     
 });
 
 module.exports = router;
