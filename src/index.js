c128a7dd11d96aac827d569ccae98cd76a822bcc
beto-dt <lucho.mix34@gmail.com>
2020-07-15 00:45:47 -0500

    Created folder src/index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/testdb",(error) =>{
+    if(!error){
+        console.log('Success');
+    }
+    else{
+        console.log('Error connecting to database.')
+    }
+
+});
