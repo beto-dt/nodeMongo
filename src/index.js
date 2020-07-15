6ca03ddf4e2fcd6f6a4fefe10bf99c5f8706f59e
beto-dt <lucho.mix34@gmail.com>
2020-07-15 00:51:59 -0500

    Updated folder src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/testdb",(error) =>{
+mongoose.connect("mongodb://localhost:27017/testdb",{useNewUrlParser:true},(error) =>{
     if(!error){
         console.log('Success');
     }
