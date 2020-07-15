4092ba262a57280040eaad5d81df35f5600179a6
beto-dt <lucho.mix34@gmail.com>
2020-07-15 02:40:56 -0500

    ok

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -6,10 +6,10 @@ const CustomerModel = mongoose.model('Customer');
 
 router.get('/list',(req, res)=>{
     //Setting
-     var customer = new CustomerModel();
+/*      var customer = new CustomerModel();
     customer.customerName ='Tito';
     customer.customerId ='3',
-    customer.save(); 
+    customer.save();  */
 
     //Getting
     CustomerModel.find((err,docs)=>{
