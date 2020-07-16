f9106519ea206c7db78e7147a5579be1ae16aca2
beto-dt <lucho.mix34@gmail.com>
2020-07-16 01:02:10 -0500

    fix2

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -4,24 +4,26 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const CustomerModel = mongoose.model('Customer');
 
-router.post('/customer/add',(req, res)=>{
+router.post('/add',(req, res)=>{
     var customer = new CustomerModel();
     customer.customerName = req.body.customerName;
     customer.customerId = Math.ceil(Math.random() *1000000000) + "";
     customer.save((err, doc) => {
         if(!err)
         {
-             res.redirect('/customer/list'); 
+               res.redirect('/customer/list');  
         }
         else
         {
             res.send('Error');
         }
-    });   
+    });
+    res.render('add-customer')
 });
 
 router.get('/list',(req, res)=>{
-    res.render('list');
+
+            res.render('list');
 });
 
 module.exports = router;
